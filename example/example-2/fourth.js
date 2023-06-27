5302820150b85c486deb968fedd88d57102eeadc
Deepaksharmadk <Deepakbdbg@gmail.com>
2023-06-27 22:13:05 +0530

    first commit

---
+++ example/example-2/fourth.js
@@ -0,0 +1,24 @@
+function firstUniqueChar(s) {
+  const charCount = {};
+  
+  // Count occurrences of each character
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Find the first unique character
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (charCount[char] === 1) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+// Test the function
+let s = "loveleetcode";
+let result = firstUniqueChar(s);
+console.log(result);
---
+++ example/second.js
@@ -0,0 +1,9 @@
+function moveZerosToEnd(nums) {
+  // No non-zero elements to move, return the input array as it is
+  return nums;
+}
+
+// Test the function
+let nums = [0];
+let result = moveZerosToEnd(nums);
+console.log(result);
